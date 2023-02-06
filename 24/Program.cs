e703d46e2cc3da998719242bbfb315fe32a70003
DanteMilten <milten47@yahoo.com>
2023-02-06 20:51:47 +0300

    24

--- 24/Program.cs
+++ 24/Program.cs
@@ -2,3 +2,13 @@
 // и выдаёт сумму чисел от 1 до A
 // a = 5
 // sum = 1 + 2 + 3 + 4 + 5
+
+Console.Clear();
+Console.Write("Введите число А: ");
+int A = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+for (int i = 1; i <= A; i++)
+{
+    sum = sum + i;
+}
+Console.WriteLine($"Результат выполнения программы = {sum}");
