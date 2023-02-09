7cc9909dfad7cdc5a34fde328f39b635dc817b04
DanteMilten <milten47@yahoo.com>
2023-02-09 19:20:07 +0300

    24

--- 24/Program.cs
+++ 24/Program.cs
@@ -3,12 +3,26 @@
 // a = 5
 // sum = 1 + 2 + 3 + 4 + 5
 
+
 Console.Clear();
-Console.Write("Введите число А: ");
-int A = Convert.ToInt32(Console.ReadLine());
-int sum = 0;
-for (int i = 1; i <= A; i++)
-{
-    sum = sum + i;
+Console.Write ("Введите число: ");
+int n = int.Parse (Console.ReadLine ());
+Console.WriteLine ($"Сумма чисел от 1 до {n} равна {GetSum (n)}");
+int GetSum (int limit) {
+    int sum = 0;
+    for (int i = 1; i <= limit; i++){
+        sum += i;
+    }
+    //Console.WriteLine($"Сумма от 1 до {limit} равна {sum}");
+    return sum;
 }
-Console.WriteLine($"Результат выполнения программы = {sum}");
+
+// Console.Clear();
+// Console.Write("Введите число А: ");
+// int A = Convert.ToInt32(Console.ReadLine());
+// int sum = 0;
+// for (int i = 1; i <= A; i++)
+// {
+//     sum = sum + i;
+// }
+// Console.WriteLine($"Результат выполнения программы = {sum}");
